1fed28332457f5879a6bc99a37975855fa70055a
Henry20000 <dangducquang999@gmail.com>
2024-08-21 23:44:36 +0700

    Product details P1

---
+++ src/components/ProductDetailsComponent/style.js
@@ -0,0 +1,14 @@
+import { Col, Image } from "antd";
+import styled from 'styled-components';
+
+export const WrapperStyleImageSmall = styled(Image)`
+   height: auto;
+   width: 100%;
+
+`
+
+export const WrapperStyleColImage = styled(Col)`
+   flex: 1;
+   margin: 0 10px;
+
+`
--- src/pages/ProductDetailsPage/ProductDetailsPage.jsx
+++ src/pages/ProductDetailsPage/ProductDetailsPage.jsx
@@ -1,8 +1,12 @@
 import React from "react";
+import ProductDetailsComponent from "../../components/ProductDetailsComponent/ProductDetailsComponent";
 
 const ProductDetailsPage = () => {
     return (
-        <div>ProductDetailsPage</div>
+       <div style={{display: 'flex', flexDirection: 'column',padding: '0 120px', background: '#efefef', height: '1000px'}}>
+         <h3>Home page</h3>
+         <ProductDetailsComponent/>
+       </div>
     )
 }
 
